1d609faf9a26989ad01b12ad1d29015a1d33441b
FARRAS-DARKUNO <abdurrachman.farras2@gmail.com>
2022-06-21 11:13:05 +0700

    init flutter

---
+++ ReactNative/src/screen/splash_screen.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+    SafeAreaView,
+    ScrollView,
+    StatusBar,
+    StyleSheet,
+    Text,
+    useColorScheme,
+    View,
+} from 'react-native';
+
+const SplashScreen = () => {
+    return (
+        <View>
+            <Text>
+                hallo Word
+            </Text>
+        </View>
+    )
+}
+
+export default SplashScreen
